840b75f94c36e7c02783fef5a043c17822a7dd72
Tanweer Badami <trbadami@yahoo.com>
2018-07-15 14:30:37 +1000

    fixed default test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,20 +8,20 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it('should create the Bus App', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Bus App'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Bus App');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bus-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Bus App!');
   }));
 });
